bb1c4b72a851d0a5149e4ec08d28080965469565
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2021-10-27 01:21:24 -0500

    Nav listo, portafolio version 1.0

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
-const menu = document.getElementById("menu");
-const nav = document.getElementById("nav");
+const menuShow = document.querySelector("#menuShow")
+const menuClose = document.querySelector("#menuClose")
+const imgGif = document.querySelector(".main__intro__img")
+const nav = document.getElementById("nav")
+const mediaquery = window.matchMedia("(min-width: 530px)")
+const skills = document.querySelector("#skills")
 
-const showMenu = () => {
+const ajusteNav = () => {
+    nav.classList.toggle("nav-hide")
+    nav.classList.toggle("navMin")
+}
+
+const mediaqueryAjuste = function() {    
+    if (mediaquery.matches){    
+        ajusteNav();
+    } else {
+        nav.classList.toggle("navMin")
+        nav.classList.toggle("nav-hide")      
+    }
+}
+
+const showMenu = () => {      
     if (nav.classList.contains("nav-hide")) {
         nav.classList.toggle("nav-hide")
-        nav.classList.toggle("header__nav")        
-    } else {
+        nav.classList.toggle("header__nav")
+        menuShow.classList.toggle("menu-hide")
+        menuShow.classList.toggle("menu__mobile")
+        menuClose.classList.toggle("menu-hide")
+        menuClose.classList.toggle("menu__mobile")        
+        imgGif.style = "opacity: 0.5";      
+    } else {        
         nav.classList.toggle("nav-hide")
-        nav.classList.toggle("header__nav")           
+        nav.classList.toggle("header__nav")                  
     }      
 }
 
-document.addEventListener("click", (e) =>{
-    
-    if (nav.classList.contains("header__nav")) {
-        nav.classList.toggle("nav-hide")
-        nav.classList.toggle("header__nav")        
-    }
+const noDisponible = () => {
+    swal({
+        title: "En construcción...",
+        text: "Proximanente disponible",
+        icon: "warning",
+      });    
+}
 
-    else if (e.target.dataset.node === "menu") {        
+if(mediaquery.matches){
+    ajusteNav();
+}
+mediaquery.addListener(mediaqueryAjuste)
+
+document.addEventListener("click", (e) =>{
+    if (nav.classList.contains("header__nav")) {        
+        nav.classList.toggle("nav-hide")
+        nav.classList.toggle("header__nav")
+        menuClose.classList.toggle("menu__mobile")
+        menuClose.classList.toggle("menu-hide")        
+        menuShow.classList.toggle("menu__mobile")
+        menuShow.classList.toggle("menu-hide")
+    } else if (e.target.dataset.node === "menu") {        
         showMenu();
-    }    
+    }
+    if(e.target.dataset.node === "noDisponible"){
+        noDisponible();
+    }
 })
